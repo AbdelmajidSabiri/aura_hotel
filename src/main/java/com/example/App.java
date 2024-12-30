2491e2dbc15c6e0070f93e90cfe8b363f1a66f42
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-12-30 17:41:35 +0100

    Create dashboard

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
-        scene = new Scene(loadFXML("home"));
+        scene = new Scene(loadFXML("dashboard"));
         stage.setScene(scene);
         stage.show();
     }
